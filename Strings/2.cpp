92dd31a6181e27c3864e493092e2ef2865615a56
kunalgavhane12 <kunalgavhane12@gmail.com>
2023-07-18 07:11:26 +0530

    code

---
+++ Strings/2.cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+StringStream
+*******************************************************************************/
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+vector<int> parseInt(string str)
+{
+    //Complete this function
+    stringstream ss(str);
+    char ch;
+    vector<int>a;
+    int temp;
+    while(ss>>temp)
+    {
+        a.push_back(temp);
+        ss>>ch;
+    }
+    return a;
+}
+int main()
+{
+    string str;
+    cin >> str;
+    vector<int> integers= parseInt(str);
+    for(int i=0;i<integers.size();i++)
+    cout<<integers[i]<<"\n";
+    
+    return 0;
+}
---
+++ Strings/3.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+Strings
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+    string a,b;
+    char c1,c2;
+    cin>>a;
+    cin>>b;
+    cout<<a.length()<<" "<<b.length()<<endl;
+    cout<<a+b<<endl;
+    c1=a[0];
+    c2=b[0];
+    a[0]=c2;
+    b[0]=c1;
+    cout<<a<<" "<<b<<endl;
+  
+    return 0;
+}
