8bfc86dd95b37ba558ee55ba729fae27ae1680e2
kunalgavhane12 <kunalgavhane12@gmail.com>
2023-07-18 18:25:02 +0530

    code

---
+++ Inheritance/2.cpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+Rectangle Area
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+/*Create classes Rectangle and RectangleArea*/
+class Rectangle{
+  protected:
+  int width,height;
+  public:
+  void read_input()
+  {
+      cin>>width>>height;
+  }
+  void display()
+  {
+      cout<<width<<" "<<height<<endl;
+  }
+};
+class RectangleArea:public Rectangle{
+  public:
+  void display()
+  {
+      cout<<width*height<<endl;
+  }
+};
+int main()
+{
+    /*Declare a RectangleArea object*/
+    RectangleArea r_area;
+    /*Read the width and height*/
+    r_area.read_input();
+    /*print the width and height*/
+    r_area.Rectangle::display();
+    /*print the area*/
+    r_area.display();
+    
+    return 0;
+}
